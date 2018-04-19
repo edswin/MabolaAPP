3c316a03cf4891f9a03654df98b05b1cdd955c87
Edwin <edswin>
2018-04-19 20:40:20 +0800

    Main project creation - added games dashboard code

--- app/src/main/java/com/example/keithgabmikay/mabolaapp/MainActivity.java
+++ app/src/main/java/com/example/keithgabmikay/mabolaapp/MainActivity.java
@@ -4,28 +4,36 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-
-        new CountDownTimer(7000, 3000) {
+        CountDownTimer start = new CountDownTimer(4000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-
-                }
+            }
 
             @Override
             public void onFinish() {
-                Intent dashboard = new Intent(MainActivity.this,Bottomnavi.class);
+                ImageView mbl = (ImageView) findViewById(R.id.mbllogoimageView);
+                Animation anim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.mytransition);
+                mbl.startAnimation(anim);
+                mbl.setVisibility(View.VISIBLE);
+                Intent dashboard = new Intent(MainActivity.this, Bottomnavi.class);
                 startActivity(dashboard);
+                finish();
             }
         }.start();
-        }
+    }
 
     }
